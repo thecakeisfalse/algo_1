843c18b2f9a869577cb94d5cafa38d8c4e925474
Semyon <52384223+thecakeisfalse@users.noreply.github.com>
2024-11-22 01:51:06 +0300

    answ

--- main.nim
+++ main.nim
@@ -1,6 +1,18 @@
-# импортируйте нужные библиотеки (советуем strutils и sequtils)
+import strutils
+import sequtils
+import sets
 
-proc main() =
-  # Реализуйте ваш код здесь
+proc main =
+  let
+    _ = stdin.readLine.parseInt
+    a = stdin.readLine.splitWhitespace.mapIt(it.parseInt)
+    k = stdin.readLine.parseInt
+    hashed = a.toHashSet
+    valid = a.filterIt(k - it in hashed)
+
+  if len(valid) == 0:
+    echo "None"
+  else:
+    echo valid[0], ' ', k-valid[0]
 
 main()
